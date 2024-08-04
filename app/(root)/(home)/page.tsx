17c0433d2369468a91ad944521a4b63510a6726b
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-04 07:31:46 +0500

    clerk authentication complete

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <section className="flex flex-col size-full gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover"></div>
+    </section>
+  );
 };
 
 export default page;
---
+++ components/NavMobile.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const NavMobile = () => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          {" "}
+          <Image
+            src="/icons/hamburger.svg"
+            width={36}
+            height={36}
+            alt="hamburger icon"
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo.svg"
+              alt="yoom logo"
+              width={32}
+              height={32}
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white ">Yoom</p>
+          </Link>
+          <div className="flex flex-col h-[calc(100vh-72px)] justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex flex-col h-full gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn(
+                          "flex items-center p-4 gap-4 rounded-lg w-full max-w-60",
+                          { "bg-blue-1": isActive }
+                        )}
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{item.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default NavMobile;
