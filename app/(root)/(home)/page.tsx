753f5ca1fefc3ad3d2a6ad02418984bb901f16ca
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-04 08:21:32 +0500

    working on home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 const page = () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
   return (
     <section className="flex flex-col size-full gap-5 text-white">
-      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover"></div>
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex flex-col justify-between h-full max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-base font-normal text-center">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-bold lg:text-6xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
