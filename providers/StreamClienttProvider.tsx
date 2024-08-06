f259db9fd1051c1710fec12c4d392d90d4d26f0e
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-06 08:36:37 +0500

    working on stream

---
+++ providers/StreamClienttProvider.tsx
@@ -0,0 +1,20 @@
+import {
+  StreamCall,
+  StreamVideo,
+  StreamVideoClient,
+  User,
+} from "@stream-io/video-react-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const userId = "user-id";
+const token = "authentication-token";
+const user: User = { id: userId };
+
+export const MyApp = () => {
+  return (
+    <div></div>
+    // <StreamVideo>
+
+    // </StreamVideo>
+  );
+};
