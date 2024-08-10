c7d99b2791425b50421db5571edf1e0fbbc56276
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-10 13:03:55 +0500

    working on meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import {
   CallControls,
   CallParticipantsList,
+  CallStatsButton,
   PaginatedGridLayout,
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
@@ -15,7 +16,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
@@ -67,10 +68,18 @@ const MeetingRoom = () => {
                 >
                   {item}
                 </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
               </div>
             ))}
           </DropdownMenuContent>
         </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
+        {/* {!isPersonalRoom && <EndCallButton />} */}
       </div>
     </section>
   );
