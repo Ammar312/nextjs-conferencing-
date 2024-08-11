68f27ef9802b1dfdc1444335321aae6faec11c2b
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-12 01:48:20 +0500

    working on personal room

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Table = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="">
+      <h1 className=""></h1>
+      <h1 className=""></h1>
+    </div>
+  );
+};
+
+const PersonalRoom = () => {
+  return <div></div>;
+};
+
+export default PersonalRoom;
