fff7eded00b956728b15cfef1a471e8d1b5f6b07
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-08-11 17:30:21 +0500

    meeting card

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,20 +1,22 @@
 "use client";
+import { useGetCall } from "@/hooks/useGetCall";
 import { CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const router = useRouter();
-  // const{endedCalls,upcomingCalls,callRecordings,isLoading} = useGetCalls()
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCall();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
   const getCalls = () => {
     switch (type) {
       case "ended":
-      // return endedCalls
+        return endedCalls;
       case "upcoming":
-      // return upcomingCalls
+        return upcomingCalls;
       case "recordings":
-      // return callRecordings
+        return callRecordings;
       default:
         return [];
     }
@@ -22,21 +24,37 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const getNoCallsMessage = () => {
     switch (type) {
       case "ended":
-      // return 'No Previous Calls'
+        return "No Previous Calls";
       case "upcoming":
-      // return 'No Upcoming Calls'
+        return "No Upcoming Calls";
       case "recordings":
-      // return 'No Recordings'
+        return "No Recordings";
       default:
         return "";
     }
   };
   useEffect(() => {
     const fetchRecordings = async () => {
-      // const callData = await Promise.all(callRecordings?.map((meeting)=>meeting.queryRecordings()) ?? []
+      const callData = await Promise.all(
+        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+      );
+      const recordings = callData
+        .filter((call) => call.recordings.length > 0)
+        .flatMap((call) => call.recordings);
+      setRecordings(recordings);
     };
-  });
-  return <div></div>;
+    if (type === "recordings") {
+      fetchRecordings();
+    }
+  }, [type, callRecordings]);
+  if (isLoading) return <Loader />;
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? "" : ""}
+    </div>
+  );
 };
 
 export default CallList;
---
+++ components/MeetingCard.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const MeetingCard = () => {
+  return <div></div>;
+};
+
+export default MeetingCard;
